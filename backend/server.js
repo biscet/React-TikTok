fc7bcd466d89cab84093a96a0fc25a512ca6fae7
Egor Prokhorov <egor.prokhorov@pinely.eu>
2022-02-02 16:42:37 +0400

    Добавление защиты от атак в backend

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ const app = express()
 const port = 9000
 
 app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeaders('Access-Control-Allow-Origin', '*'), res.setHeaders('Access-Control-Allow-Headers', '*'), next()
+})
 
 const connection_url =
   'mongodb+srv://admin:B2fVqL5kAdhUJP09@cluster0.dkdlh.mongodb.net/tiktok?retryWrites=true&w=majority'
@@ -20,6 +23,16 @@ mongoose.connect(connection_url, {
 app.get('/', (req, res) => res.status(200).send('Hellow World'))
 
 app.get('/v1/posts', (req, res) => res.status(200).send(data))
+
+app.get('/v2/posts', (req, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).send(data)
+    }
+  })
+})
 
 app.post('/v2/posts', (req, res) => {
   const dbVideos = req.body
