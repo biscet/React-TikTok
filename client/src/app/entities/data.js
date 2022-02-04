47defaa62b8205328418c695d509692563664dcb
Egor Prokhorov <egor.prokhorov@pinely.eu>
2022-02-04 12:05:53 +0400

    Дабвил временную data

---
+++ client/src/app/entities/data.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    url: 'https://www.w3schools.com/html/mov_bbb.mp4',
+    channel: 'biscet',
+    song: 'eto posto ahuenno',
+    likes: '435',
+    messages: '14',
+    description: 'Wow that react app',
+    shares: '2'
+  },
+  {
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+    channel: 'heroki',
+    song: 'paty na hate',
+    likes: '6135',
+    messages: '496',
+    description: 'FAKE SITUATION COMEBACK',
+    shares: '262'
+  },
+  {
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
+    channel: 'heroki',
+    song: 'paty na hate',
+    likes: '731351',
+    messages: '14232',
+    description: 'FAKE SITUATION',
+    shares: '21'
+  },
+  {
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
+    channel: 'heroki',
+    song: 'paty na hate',
+    likes: '43135',
+    messages: '14',
+    description: 'FAKE SITUATION',
+    shares: '2'
+  }
+]
+
+export default data
--- client/src/app/pages/container/container.jsx
+++ client/src/app/pages/container/container.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState, useRef } from 'react'
 
+import data from '@entities/data.js'
 import axios from '@utils/requester'
 import { Video } from '@app/components/container'
 import './container.scss'
 
 export default function Container() {
   //Массив с видео роликами
-  const [videos, setVideos] = useState([])
+  const [videos, setVideos] = useState(data)
 
   //Направление и высота скролла, нужна для логики проигрывания видео
   const [scroll, setScroll] = useState(0)
@@ -17,14 +18,12 @@ export default function Container() {
 
   //Принятие данных с сервера
   useEffect(() => {
-    async function fetchPosts() {
-      const response = await axios.get('/v2/posts')
-      setVideos(response.data)
-
-      return response
-    }
-
-    fetchPosts()
+    // async function fetchPosts() {
+    //   const response = await axios.get('/v2/posts')
+    //   setVideos(response.data)
+    //   return response
+    // }
+    // fetchPosts()
   }, [])
 
   //Отслеживание скролла и направление скролла
@@ -49,7 +48,7 @@ export default function Container() {
           id={key}
           scroll={scroll}
           goingUp={goingUp}
-          key={video._id}
+          key={key}
           shares={video.shares}
           messages={video.messages}
           likes={video.likes}
